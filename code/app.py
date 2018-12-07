6080e2dc81a4957f481fbd0116b69672e76dbb70
S Fujimoto <unicoshun@gmail.com>
2018-12-07 22:06:49 +0900

    ~66

--- code/app.py
+++ code/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
+
+from user import UserRegister
 
 app = Flask(__name__)
 app.secret_key = "sf"
@@ -14,6 +16,11 @@ items = []
 
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        "price", type=float, required=True, help="This field cannot be left blank"
+    )
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x["name"] == name, items), None)
@@ -25,8 +32,8 @@ class Item(Resource):
                 {"message": "An item with name '{}' already exists.".format(name)},
                 400,
             )
+        data = Item.parser.parse_args()
 
-        data = request.get_json()
         item = {"name": name, "price": data["price"]}
         items.append(item)
         return item, 201
@@ -37,7 +44,8 @@ class Item(Resource):
         return {"message": "Item deleted"}
 
     def put(self, name):
-        data = request.get_json()
+        data = Item.parser.parse_args()
+
         item = next(filter(lambda x: x["name"] == name, items), None)
         if item is None:
             item = {"name": name, "price": data["price"]}
@@ -54,5 +62,6 @@ class ItemList(Resource):
 
 api.add_resource(Item, "/item/<string:name>")
 api.add_resource(ItemList, "/items")
+api.add_resource(UserRegister, "/register")
 
 app.run(port=5000, debug=True)
--- code/user.py
+++ code/user.py
@@ -1,6 +1,69 @@
+import sqlite3
+from typing import List
+from sqlite3 import Connection, Cursor
+from flask_restful import Resource, reqparse
+
+
 class User:
-    def __init__(self, _id, username, password):
+    def __init__(self, _id, username, password) -> None:
         self.id = _id
         self.username = username
         self.password = password
 
+    @classmethod
+    def find_by_username(cls, username) -> "User":
+        connection: Connection = sqlite3.connect("data.db")
+        cursor: Cursor = connection.cursor()
+
+        query: str = "SELECT * FROM users WHERE username=?"
+        result: Cursor = cursor.execute(query, (username,))
+        row: List = result.fetchone()
+
+        if row:
+            user: User = cls(*row)
+        else:
+            user = None
+
+        connection.close()
+        return user
+
+    @classmethod
+    def find_by_id(cls, id) -> "User":
+        connection: Connection = sqlite3.connect("data.db")
+        cursor: Cursor = connection.cursor()
+
+        query: str = "SELECT * FROM users WHERE id=?"
+        result: Cursor = cursor.execute(query, (id,))
+        row: List = result.fetchone()
+
+        if row:
+            user: "User" = cls(*row)
+        else:
+            user = None
+
+        connection.close()
+        return user
+
+
+class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        "username", type=str, required=True, help="This field cannot be left blank"
+    )
+    parser.add_argument(
+        "password", type=str, required=True, help="This field cannot be left blank"
+    )
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+
+        query = "INSERT INTO users VALUES (NULL, ?, ?)"  # NULL as id
+        cursor.execute(query, (data["username"], data["password"]))
+
+        connection.commit()
+        connection.close()
+
+        return {"message": "User created successfully."}, 201
